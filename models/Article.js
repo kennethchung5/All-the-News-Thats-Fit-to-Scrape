c0354c2fad3c116a48635e57135d147a0c143773
Kenneth Chung <kennethchung5@gmail.com>
2019-02-24 00:59:30 -0500

    added models and scraping functionality

---
+++ models/Article.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ArticleSchema = new Schema({  
+  headline: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String
+    // required: true
+  },
+  summary: {
+    type: String
+    // required: true
+  },  
+  url: {
+    type: String,
+    required: true
+  },
+  comments: {
+    type: Schema.Types.ObjectId,
+    ref: "Comment"
+  }
+});
+
+const Article = mongoose.model("Article", ArticleSchema);
+
+module.exports = Article;
--- server.js
+++ server.js
@@ -19,7 +19,49 @@ app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
 //connect to mongoDB (decide on a name first)
-mongoose.connect;
+mongoose.connect("mongodb://localhost/newsArticlesDB", { useNewUrlParser: true });
+
+
+app.get("/scrape", (req, res) => {
+    // console.log("The scrape route is hit!")    
+    axios.get("https://www.nytimes.com/section/technology").then(response => {
+      
+      const $ = cheerio.load(response.data);  
+      
+      $("li article div.css-10wtrbd").each((i, element) => {
+        
+        const newArticle = {};  
+        
+        newArticle.headline = $(element).children("h2").children("a").text();
+        newArticle.author = $(element).find("[itemprop=name]").text();
+
+        // do some string manipulation to remove the By [author] at end of summary.
+        let newSummary = $(element).find("p").text();        
+
+        if (newSummary.lastIndexOf(".By ") > -1) {
+            newSummary = newSummary.substring(0, newSummary.lastIndexOf(".By ") + 1);
+        }        
+
+        newArticle.summary = newSummary;        
+        
+        // href values omit the "https://www.nytimes.com/" at the beginning. Is there a more elegant way to get the full path than hardcoding the beginning?
+        newArticle.url = "https://www.nytimes.com" + $(element).children("h2").children("a").attr("href");
+  
+        
+        db.Article.create(newArticle)
+          .then(dbArticle => {
+            console.log(dbArticle);
+          })
+          .catch(error => {            
+            console.log(error);
+          });
+      });
+  
+      res.send("Scrape Complete");
+    });
+  });
+
+
 
 
 
